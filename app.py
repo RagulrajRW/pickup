d902289b19d45f645f1dd4d72b337f92fcd0269e
RagulrajRW <ragulraj@randomwalk.ai>
2025-01-08 00:02:57 +1100

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def generate_pickup_lines(topic, flirtiness):
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=False, host='0.0.0.0', port=5001)
 
 
 
